8a85fa445c3b44222f1196134921c4aedeac0387
kyungkoo <rudrn85@gmail.com>
2025-01-02 17:34:34 +0900

    add recyclerview template

--- BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/MainActivity.kt
+++ BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/MainActivity.kt
@@ -1,14 +1,40 @@
 package com.bignerdranch.android.beatbox
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bignerdranch.android.beatbox.databinding.ActivityMainBinding
+import com.bignerdranch.android.beatbox.databinding.ListItemSoundBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.recyclerView.apply {
+            layoutManager = GridLayoutManager(context, 3)
+            adapter = SoundAdapter()
+        }
+    }
+
+    // RecyclerView Helper Classes
+    private inner class SoundHolder(private val binding: ListItemSoundBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    private inner class SoundAdapter(): RecyclerView.Adapter<SoundHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SoundHolder {
+            val binding = DataBindingUtil.inflate<ListItemSoundBinding>(layoutInflater, R.layout.list_item_sound, parent, false)
+            return SoundHolder(binding)
+        }
+
+        override fun getItemCount(): Int = 0
+
+        override fun onBindViewHolder(holder: SoundHolder, position: Int) {
+            TODO("Not yet implemented")
+        }
     }
 }
