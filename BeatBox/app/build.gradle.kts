3e4369092de18236f5390e50feda38cc4d690bf6
kyungkoo <rudrn85@gmail.com>
2025-01-07 00:21:41 +0900

    add ch20

--- BeatBox/app/build.gradle.kts
+++ BeatBox/app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.recyclerview)
     testImplementation(libs.junit)
+    testImplementation(libs.mokito.core)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
