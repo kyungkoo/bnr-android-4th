f2e595ebc33bfc6a3e1ef82029536fd901708734
kyungkoo <rudrn85@gmail.com>
2025-01-06 18:36:36 +0900

    add sound

--- BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.kt
+++ BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.kt
@@ -1,14 +1,20 @@
 package com.bignerdranch.android.beatbox
 
 import android.content.res.AssetManager
+import android.media.SoundPool
 import android.util.Log
+import java.io.IOException
 
 private const val TAG = "BeatBox"
 private const val SOUND_FOLDER = "sample_sounds"
+private const val MAX_SOUNDS = 5
 
 class BeatBox(private val assets: AssetManager) {
 
     val sounds: List<Sound>
+    private val soundPool = SoundPool.Builder()
+        .setMaxStreams(MAX_SOUNDS)
+        .build()
 
     init {
         sounds = loadSounds()
@@ -17,12 +23,34 @@ class BeatBox(private val assets: AssetManager) {
     private fun loadSounds(): List<Sound> {
         try {
             val soundNames = assets.list(SOUND_FOLDER) ?: emptyArray()
-            return soundNames
+            val sounds = soundNames
                 .map { Sound("$SOUND_FOLDER/$it") }
                 .toList()
+
+            sounds.forEach {
+                try {
+                    load(it)
+                } catch (ioe: IOException) {
+                    Log.e(TAG, "Could not load sound $it", ioe)
+                }
+            }
+
+            return sounds
         } catch (e: Exception) {
             Log.e(TAG, "Could not list assets", e)
             return emptyList()
+        }
+    }
+
+    private fun load(sound: Sound) {
+        val afd = assets.openFd(sound.assetPath)
+        val soundId = soundPool.load(afd, 1)
+        sound.soundId = soundId
+    }
+
+    fun play(sound: Sound) {
+        sound.soundId?.let {
+            soundPool.play(it, 1.0f, 1.0f, 1, 0, 1.0f)
         }
     }
 }
